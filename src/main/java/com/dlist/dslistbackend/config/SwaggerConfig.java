79d4f502f0b1ce4fcf2e99693741610492afe4ff
GUstavo <almeidagustavo2015@outlook.com>
2023-05-31 19:43:30 -0300

    SwaggerConfig

---
+++ src/main/java/com/dlist/dslistbackend/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.dlist.dslistbackend.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import jakarta.servlet.ServletContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI(ServletContext servletContext) {
+        Server server = new Server().url(servletContext.getContextPath());
+        return new OpenAPI()
+                .servers(List.of(server));
+        // ...
+    }
+}
